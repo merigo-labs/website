98718628cd506571e4301d21e7ab6dbb00759b35
MerigoHeraby <merigo.heraby@gmail.com>
2023-02-12 15:57:48 +0000

    Add Asset Links

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   Widget iconLink(final String name, { required final String url }) => InkWell(
     child: Image.asset(
-      'assets/icons/$name',
+      'icons/$name',
       width: 32.0, height: 32.0,
     ),
     onTap: () => launchUrlString(url),
@@ -23,62 +23,64 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Merigo Labs',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          'Merigo Labs',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 32, color: Colors.white),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 8.0),
-                          child: Text(
-                            'Building tools and dApps for the Solana ecosystem.',
+      routes: {
+        '/': (context) => Scaffold(
+          backgroundColor: Colors.black,
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Merigo Labs',
                             textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 16, color: Colors.white60),
+                            style: TextStyle(fontSize: 32, color: Colors.white),
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              'Building tools and dApps for the Solana ecosystem.',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 16, color: Colors.white60),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    iconLink(
-                      'github64.png', 
-                      url: 'https://github.com/merigo-labs/solana-web3',
-                    ),
-                    const SizedBox(
-                      width: 24.0,
-                    ),
-                    iconLink(
-                      'twitter300.png', 
-                      url: 'https://twitter.com/MerigoLabs',
-                    ),
-                  ],
-                ),
-              ],
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      iconLink(
+                        'github64.png', 
+                        url: 'https://github.com/merigo-labs/solana-web3',
+                      ),
+                      const SizedBox(
+                        width: 24.0,
+                      ),
+                      iconLink(
+                        'twitter300.png', 
+                        url: 'https://twitter.com/MerigoLabs',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      }, 
     );
   }
 }
